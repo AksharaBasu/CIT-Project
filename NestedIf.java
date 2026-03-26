2416c3c71fbb55e274b68b72c9fbd7a075cf53e4
AksharaBasu <aksharabasu167@gmail.com>
2026-03-26 12:22:32 +0530

    update nestedif.java

--- NestedIf.java
+++ NestedIf.java
@@ -8,11 +8,11 @@ public class NestedIf {
         if(age>=65){
             System.out.println("Eligible to avail govement fund");
         }else
-                System.out.println("we are not eligible for vote and avail govement scheme ");
+                System.out.println("we are  eligible for vote but not to avail govement scheme ");
 
         } 
         else
-            System.out.println("we are not eligible to vote");
+            System.out.println("we are not eligible to vote not eligible to avail govement scheme");
         sc.close();
 
         }
