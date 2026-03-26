dcf43afe038547066a8d654858de52ce8e809a26
AksharaBasu <aksharabasu167@gmail.com>
2026-03-26 10:18:29 +0530

    updated Constrol Structurce

---
+++ ControlStructure.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ControlStructure {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.err.println("enter the age");
+        // int age =sc.nextInt();
+        // if(age>=18)
+        //     System.out.println("eligible to vote");
+        // System.out.println("not elegible");
+        // sc.close();
+        //if -else'biggest of two number'
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter first number");
+            int a = sc.nextInt();
+            System.out.println("enter the second number");
+            int b = sc.nextInt();
+            if(a>b)
+            System.out.println("first is largerst number");
+            else
+            System.out.println("second is biggest");
+
+
+
+    }
+        
+    }
